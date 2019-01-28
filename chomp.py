3778134160286f28e686b42320a3fa9dccbd4bac
JeremyEspinoza3912 <43081346+JeremyEspinoza3912@users.noreply.github.com>
2019-01-28 18:26:29 -0500

    added comments

--- chomp.py
+++ chomp.py
@@ -9,61 +9,152 @@ import random
 EMOJI = {-1: '\u2612', 0: ' ', 1: '\u2610'}
 
 
+
+
+
 class ChompGame:
-    def __init__(self, size=(3, 4)):
-        self.p1 = Player()
-        self.p2 = Player()
-        self.turn = random.choice([self.p1, self.p2])
+
+    """Contains the control flow for the game"""
+
+    def __init__(self, n_players=2, size=(3, 4)):
+
+        self.n_players = n_players
+
+        self.size = size
+
+        self.board = Board(*size)
+
+        self.game_over = False
+
+        self.players = []
+
+        self.current_player = None
+
+
 
     def __repr__(self):
-        pass
+
+        return f'ChompGame({self.n_players}, {self.size})'
+
+
 
     def play(self):
-        players= self.p1 or self.p2
-        if self.turn == players:
-            return Player(self.name)
-            f'You must make the first move'
 
-        players_choice= take from Board
+        self.setup()
 
-        while players_choice =! self.state[-1][0]:
-            return f'Next Players Turn'
-        else:
-            return f'GAME OVER'
+        while not self.game_over:
+
+            print(f'{self.current_player}, it\'s your turn!\n')
+
+            print(self.board)
+
+            self.move()
+
+            if self.board.state[-1][0] == 0:
+
+                self.game_over = True
+
+                self.current_player.wins += 1
+
+
+
+    def setup(self):
+
+        for i in range(1, self.n_players + 1):
+
+            print(f'***Player {i}***')
+
+            self.players.append(Player())
+
+
+
+        self.current_player = random.choice(self.players)
+
+
+
+    def move(self):
+
+        coord_str = input("Enter the coordinates for your move. (e.g. A3)")
+
+        row_str, col_str = coord_str[0].upper(), coord_str[1]
+
+        row = ord(row_str) - 65
+
+        col = int(col_str)
+
+        self.board.take(row, col)
+
 
 
 
 
 class Board:
+
     def __init__(self, rows, cols):
+
         # Use a 2d array to store board state
+
         # ones for chocolate, zeros for eaten squares, and -1 for poison
+
         self.rows = rows
+
         self.cols = cols
+
         self.state = np.ones((rows, cols), dtype=int)
+
         self.state[-1][0] = -1
 
+
+
     def __repr__(self):
+
         return f'Board({self.rows}, {self.cols})'
 
+
+
     def __str__(self):
+
         col_idx = range(self.cols)
-        row_idx = [chr(letter) for letter in range(65, 65 + self.rows)]
+
+        row_idx = [chr(letter) for letter in range(65, 65+self.rows)]
+
         board_emoji = np.array([[EMOJI[val] for val in row] for row in self.state])
+
         board_df = pd.DataFrame(data=board_emoji, index=row_idx, columns=col_idx)
+
         return str(board_df)
 
+
+
     def take(self, row, col):
-        for r in range(row + 1):
+
+        # self.state[:row+1, col:] = 0
+
+        for r in range(row+1):
+
             self.state[r][col:] = 0
 
 
 
-    
+
+
 class Player:
-    def __init__(self, score=0, name= 'none'):
-        self.score = score
-        self.name = input('Enter your name')
+
+    def __init__(self):
+
+        self.name = input("\tEnter your name: ")
+
+        self.wins = 0
+
+
 
     def __repr__(self):
-        return f'Player(score={self.score},names={self.names})'
+
+        return f'Player({self.name})'
+
+
+
+    def __str__(self):
+
+        return self.name
+
