5edaebaecb1b7af14a35ef4802a4255644268c0e
kevingalarraga <43154310+kevingalarraga@users.noreply.github.com>
2019-01-29 23:05:26 -0500

    worked on play function

--- chomp.py
+++ chomp.py
@@ -4,9 +4,7 @@ import pandas as pd
 import random
 
 
-
 EMOJI = {-1: '\u2612', 0: ' ', 1: '\u2610'}
-
 
 
 class ChompGame:
@@ -22,19 +20,25 @@ class ChompGame:
     def __repr__(self):
         return f'ChompGame({self.n_players}, {self.size})'
 
-    
     def play(self):
         self.setup()
         while not self.game_over:
-            print(f'{self.current_player}, it\'s your turn!\n')
+            print(f'{self.current_player}, it\'s your turn Player 1!\n')
             print(self.board)
             self.move()
-            if self.board.state[-1][0] == 0:
-                self.game_over = True
-                print(f'{self.current_player}, You Lost')
+            if self.board.state[-1][0] != 0:
+                self.game_over = False
+                print(f'{self.current_player}, You have moved ')
+                p = self.players
+                i = 1
+                for players in range(2):
+                    print(f'{p[i % 2]}, it\'s your turn Player 2!\n')
+                    i += 1
+                    
             else:
-                pass
-
+                if self.board.state[-1][0] == 0:
+                    self.game_over = True
+                    print(f'{self.current_player}, You have lost, GAME OVER XD ')
 
     def setup(self):
         for i in range(1, self.n_players + 1):
@@ -42,9 +46,7 @@ class ChompGame:
             print(f'***Player {i}***')
             self.players.append(Player())
 
-
-        self.current_player = random.choice(self.players).cycle
-
+        self.current_player = random.choice(self.players)
 
     def move(self):
         coord_str = input("Enter the coordinates for your move. (e.g. A3)")
@@ -61,25 +63,20 @@ class Board:
         # Use a 2d array to store board state
 
         # ones for chocolate, zeros for eaten squares, and -1 for poison
-
         self.rows = rows
         self.cols = cols
         self.state = np.ones((rows, cols), dtype=int)
         self.state[-1][0] = -1
 
-
     def __repr__(self):
         return f'Board({self.rows}, {self.cols})'
 
-
     def __str__(self):
-
         col_idx = range(self.cols)
-        row_idx = [chr(letter) for letter in range(65, 65+self.rows)
+        row_idx = [chr(letter) for letter in range(65, 65+self.rows)]
         board_emoji = np.array([[EMOJI[val] for val in row] for row in self.state])
         board_df = pd.DataFrame(data=board_emoji, index=row_idx, columns=col_idx)
         return str(board_df)
-
 
     def take(self, row, col):
         # self.state[:row+1, col:] = 0
@@ -97,3 +94,4 @@ class Player:
 
     def __str__(self):
         return self.name
+
